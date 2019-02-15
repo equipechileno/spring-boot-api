853f9a0119f92a3eb89ee33d5f7b982daae6ec25
Marcos Boeing <marcos.goncalves@resourceit.com>
2019-02-15 17:04:21 -0200

    Inclusão de PutMapping

--- src/main/java/com/resource/demo/http/HelloWorldController.java
+++ src/main/java/com/resource/demo/http/HelloWorldController.java
@@ -4,7 +4,11 @@ import com.resource.demo.http.data.HelloRequest;
 import com.resource.demo.repository.MessageEntity;
 import com.resource.demo.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/messages")
@@ -34,5 +38,22 @@ public class HelloWorldController {
     public MessageEntity findById(@PathVariable Long id) {
 
         return messageRepository.findById(id).get();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MessageEntity> putMessageEntity(@PathVariable Long id, @RequestBody HelloRequest request){
+
+        Optional<MessageEntity> optionalMessageEntity = messageRepository.findById(id);
+
+        if (!optionalMessageEntity.isPresent()){
+            return ResponseEntity.badRequest().build();
+        }
+
+        MessageEntity messageEntity = optionalMessageEntity.get();
+
+        messageEntity.setMessage(String.format(request.getMessage()));
+
+        return ResponseEntity.ok(messageRepository.save(messageEntity));
+
     }
 }
